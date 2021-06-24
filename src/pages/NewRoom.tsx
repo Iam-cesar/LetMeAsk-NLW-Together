64ae4151f66b6ed50b959228afdcfaf25b7ffc33
Cesar Augusto <56457040+cesar-abj@users.noreply.github.com>
2021-06-23 21:20:49 -0300

    add autenticação e inserção de perguntas

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,23 +1,45 @@
-import { Link } from 'react-router-dom'
+import { FormEvent, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 
 import illustration from '../assets/images/illustration.svg'
 import logoApp from '../assets/images/logo.svg'
 import '../styles/auth.scss'
 
 import { Button } from '../components/Button'
-import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
 
+import { useAuth } from '../hooks/useAuth'
 
 export function NewRoom(){
 
 	const {user} = useAuth()
+
+	const [newRoom, setNewRoom] = useState('')
+	
+	const history = useHistory()
+
+	async function handleCreateRoom(e: FormEvent){
+		e.preventDefault()
+
+		if (newRoom.trim() === ''){
+			return
+		}
+
+		const roomRef = database.ref('rooms')
+
+		const firebaseRoom = await roomRef.push({
+			title: newRoom,
+			authorId: user?.id,
+		})
+
+		history.push(`/rooms/${firebaseRoom.key}`)
+	}
 
 	return(
 		<div id='page-auth'>
 
 			<aside>
 				<img src={illustration} alt="simbolizando perguntas e respostas" />
-				<h1>{user?.name}</h1>
 				<strong>Toda pergunta tem uma resposta.</strong>
 				<p>Aprenda e compartilhe conhecimento com outras pessoas</p>
 			</aside>
@@ -27,10 +49,12 @@ export function NewRoom(){
 					<img src={logoApp} alt="letmeask" />
 					<h2>Criar uma nova sala</h2>
 
-					<form>
+					<form onSubmit={handleCreateRoom}>
 						<input
 							type="text"
 							placeholder="Nome da sala"
+							onChange={event => setNewRoom(event.target.value)}
+							value={newRoom}
 						/>
 
 						<Button type="submit">
